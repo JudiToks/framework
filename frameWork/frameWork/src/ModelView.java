f1cfcbf29445d7fe4f398c927426a0b9c4d9d8d6
JudiToks <judi.rakotoarisedy@gmail.com>
2023-07-04 20:05:11 +0300

    sprint6

--- frameWork/frameWork/src/ModelView.java
+++ frameWork/frameWork/src/ModelView.java
@@ -1,8 +1,11 @@
 package etu1820.framework;
 
+import java.util.HashMap;
+
 public class ModelView 
 {
     String url;
+    HashMap<String, Object> data = new HashMap<>();
 
     // getters & setters
     public String getUrl() 
@@ -13,10 +16,26 @@ public class ModelView
     {
         this.url = url;
     }
+    public HashMap<String, Object> getData()
+    {
+        return data;
+    }
+    public void setData(HashMap<String, Object> data)
+    {
+        this.data = data;
+    }
 
     // constructor
+    public ModelView() {}
     public ModelView(String url) 
     {
         this.url = url;
+    }
+
+    public void addItem(String key, Object value)
+    {
+        HashMap<String, Object> temp = new HashMap<>();
+        temp.put(key, value);
+        this.setData(temp);
     }
 }
