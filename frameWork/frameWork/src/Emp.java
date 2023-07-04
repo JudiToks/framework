ff5d823e7965510dd5589312323f7fe9ab222d48
JudiToks <judi.rakotoarisedy@gmail.com>
2023-07-04 11:39:23 +0300

    sprint3

---
+++ frameWork/frameWork/src/Emp.java
@@ -0,0 +1,12 @@
+package etu1820.framework.util;
+
+import etu1820.framework.annotation.*;
+
+public class Emp 
+{
+    @Annotation(url = "/getEmp")
+    public void getEmp()
+    {
+
+    }
+}
--- frameWork/frameWork/src/FrontServlet.java
+++ frameWork/frameWork/src/FrontServlet.java
@@ -2,28 +2,42 @@ package etu1820.framework.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.util.HashMap;
+import java.util.Vector;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import etu1820.framework.Mapping;
+import etu1820.framework.util.Code;
 
 public class FrontServlet extends HttpServlet
 {
 
     HashMap<String, Mapping> mappingUrls;
 
-    public void init() throws ServletException
+    @Override
+    public void init()
     {
         try 
         {
+            Vector<String> allPath = Code.readPackage(getServletContext().getResource(".").toURI().getPath() + "WEB-INF/classes");
             mappingUrls = new HashMap<>();
+            for (String path : allPath)
+            {
+                Vector<Code> codes = Code.readAnnotation(path);
+                for (Code code : codes)
+                {
+                    mappingUrls.put(code.getValue(), new Mapping(code.getClassName(), code.getMethod()));
+                }
+            }
         } 
-        catch (Exception e) 
+        catch (URISyntaxException | MalformedURLException e) 
         {
-            throw e;
+            throw new RuntimeException(e);
         }
     }
 
@@ -39,6 +53,7 @@ public class FrontServlet extends HttpServlet
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet FrontServlet at " + request.getContextPath() + "</h1>");
+            // out.println("<p>" + mappingUrls.get("/getEmp").getClassName() + "</p>");
             out.println("</body>");
             out.println("</html>");
         }
@@ -55,10 +70,5 @@ public class FrontServlet extends HttpServlet
     {
         processRequest(request, response);
     }
-
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
